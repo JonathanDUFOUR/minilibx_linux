aa1ffa674a4dbf43b593d77bce67c401fe3f6195
jodufour <jodufour@student.42.fr>
2021-08-07 09:54:47 +0200

    Reput empty function int_param_undef in lookup both time, as was it at the origin, maybe ?

--- private/mlx_event_lookup.h
+++ private/mlx_event_lookup.h
@@ -6,7 +6,7 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/04 22:59:04 by jodufour          #+#    #+#             */
-/*   Updated: 2021/08/05 21:26:29 by jodufour         ###   ########.fr       */
+/*   Updated: 2021/08/07 09:52:18 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include "mlx_int.h"
 
+void	mlx_int_param_undef();
 void	mlx_int_param_KeyPress(t_xvar *xvar, XEvent *ev, t_win_list *win);
 void	mlx_int_param_KeyRelease(t_xvar *xvar, XEvent *ev, t_win_list *win);
 void	mlx_int_param_ButtonPress(t_xvar *xvar, XEvent *ev, t_win_list *win);
@@ -25,6 +26,8 @@ void	mlx_int_param_generic(t_xvar *xvar, XEvent *ev, t_win_list *win);
 
 void	(*(g_mlx_int_param_event[]))() =
 {
+	mlx_int_param_undef,
+	mlx_int_param_undef,
 	mlx_int_param_KeyPress,
 	mlx_int_param_KeyRelease,
 	mlx_int_param_ButtonPress,
